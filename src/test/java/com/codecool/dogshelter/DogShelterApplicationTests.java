79f5ac0ab2b77abae14acf54997430d435e9ff5f
Adam Gonda <adamgondagyula@gmail.com>
2019-03-11 11:33:15 +0100

    initial commit

---
+++ src/test/java/com/codecool/dogshelter/DogShelterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.dogshelter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogShelterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
