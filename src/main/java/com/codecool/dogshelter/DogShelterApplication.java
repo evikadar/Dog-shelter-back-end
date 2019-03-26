10e6a3acd7908e74990d0aab2a3a1e25e639b248
Nora Filep <filepnora@gmail.com>
2019-03-26 13:56:55 +0100

    more data for testing

--- src/main/java/com/codecool/dogshelter/DogShelterApplication.java
+++ src/main/java/com/codecool/dogshelter/DogShelterApplication.java
@@ -1,8 +1,10 @@
 package com.codecool.dogshelter;
 
-import com.codecool.dogshelter.model.Shelter;
+import com.codecool.dogshelter.model.shelter.Address;
+import com.codecool.dogshelter.model.shelter.Shelter;
 import com.codecool.dogshelter.model.dog.Breed;
 import com.codecool.dogshelter.model.dog.Dog;
+import com.codecool.dogshelter.repository.AddressRepository;
 import com.codecool.dogshelter.repository.DogRepository;
 import com.codecool.dogshelter.repository.ShelterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,9 @@ public class DogShelterApplication {
     @Autowired
     private ShelterRepository shelterRepository;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(DogShelterApplication.class, args);
     }
@@ -30,10 +35,17 @@ public class DogShelterApplication {
     @Bean
     public CommandLineRunner init() {
         return args -> {
-            Dog bobby = Dog.builder().age(5).breed(Breed.CHIHUAHUA).name("Bobby").build();
-            dogRepository.save(bobby);
+            //Dog bobby = Dog.builder().age(5).breed(Breed.CHIHUAHUA).name("Bobby").build();
 
-            Shelter noe = Shelter.builder().name("Noé Állatotthon").build();
+            //Address noe_address = Address.builder().country("Hungary").city("Budapest").address("Csordakút út 23.")
+            //        .zipCode(1171).build();
+
+            Shelter noe = Shelter.builder().email("noe@gmail.com").phoneNumber("555-7777").name("Noé Állatotthon")
+                    //.address(noe_address).dog(bobby)
+                    .build();
+
+
+            //bobby.setShelter(noe);
             shelterRepository.save(noe);
 
         };
