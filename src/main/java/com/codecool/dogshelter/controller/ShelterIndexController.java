961d49181cb2247ab5d107148f41b553eec835bf
Nora Filep <filepnora@gmail.com>
2019-03-26 14:00:32 +0100

    query for getting shelter data

--- src/main/java/com/codecool/dogshelter/controller/ShelterIndexController.java
+++ src/main/java/com/codecool/dogshelter/controller/ShelterIndexController.java
@@ -1,12 +1,14 @@
 package com.codecool.dogshelter.controller;
 
 
+import com.codecool.dogshelter.model.shelter.Shelter;
 import com.codecool.dogshelter.model.dog.Dog;
 import com.codecool.dogshelter.repository.DogRepository;
 import com.codecool.dogshelter.repository.ShelterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,6 +27,12 @@ public class ShelterIndexController {
     private List<Dog> shelterDogs() {
         // TODO: write query for this to return dogs of certain shelters
         return dogRepository.findAll();
+    }
+
+    @GetMapping("/shelter/{id}")
+    private Shelter shelterDetails(@PathVariable Long id){
+        Shelter shelterDetailsById = shelterRepository.getShelterDetailsById(id);
+        return shelterDetailsById;
     }
 
 }
--- src/main/java/com/codecool/dogshelter/repository/ShelterRepository.java
+++ src/main/java/com/codecool/dogshelter/repository/ShelterRepository.java
@@ -1,7 +1,13 @@
 package com.codecool.dogshelter.repository;
 
-import com.codecool.dogshelter.model.Shelter;
+import com.codecool.dogshelter.model.shelter.Shelter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShelterRepository extends JpaRepository<Shelter, Long> {
+
+    @Query("SELECT s FROM Shelter s where s.id = :shelter_id")
+    Shelter getShelterDetailsById(@Param("shelter_id") Long id);
+
 }
