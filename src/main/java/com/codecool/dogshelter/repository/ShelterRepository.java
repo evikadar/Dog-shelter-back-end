c84161f86ab300982266e90200e5b52b2602c0fb
Nora Filep <filepnora@gmail.com>
2019-04-11 15:24:02 +0200

    unused methods deleted

--- src/main/java/com/codecool/dogshelter/repository/ShelterRepository.java
+++ src/main/java/com/codecool/dogshelter/repository/ShelterRepository.java
@@ -15,16 +15,4 @@ public interface ShelterRepository extends JpaRepository<Shelter, Long> {
     @Query("SELECT s FROM Shelter s where s.id = :shelter_id")
     List<ShelterDetails> findByShelterId(@Param("shelter_id") Long id);
 
-    @Query("UPDATE Shelter s set s.name = :name, s.email = :email, s.phoneNumber = :phoneNumber, s.shelterDescription = :description WHERE s.id = :id")
-    @Transactional
-    @Modifying(clearAutomatically = true)
-    void updateShelterDetailsById(@Param("id") Long id, @Param("name") String name, @Param("email") String email, @Param("phoneNumber") String phoneNumber,
-                                  @Param("description") String shelterDescription);
-
-    @Query("UPDATE Address a set a.country = :country, a.city = :city, a.address = :address, a.zipCode = :zipCode WHERE a.id = :id")
-    @Transactional
-    @Modifying(clearAutomatically = true)
-    void updateAddressById(@Param("id") Long id, @Param("country") String country, @Param("city") String city, @Param("address") String address,
-                                  @Param("zipCode") Integer zipCode);
-
 }
