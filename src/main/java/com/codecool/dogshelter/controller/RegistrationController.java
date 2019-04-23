628a9cc6b9e400970097d0e264becb105649e380
Akos Kolloros <kollorosa@gmail.com>
2019-04-23 13:40:50 +0200

    Hashing is working

--- src/main/java/com/codecool/dogshelter/controller/RegistrationController.java
+++ src/main/java/com/codecool/dogshelter/controller/RegistrationController.java
@@ -3,11 +3,15 @@ package com.codecool.dogshelter.controller;
 import com.codecool.dogshelter.model.User;
 import com.codecool.dogshelter.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 public class RegistrationController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -18,7 +22,7 @@ public class RegistrationController {
         // TODO: HASH THEM BEFORE SAVE
         String username = newUser.getUsername();
         String email = newUser.getEmail();
-        String pw1 = newUser.getPassword1();
+        String pw1 = passwordEncoder.encode(newUser.getPassword1());
         User newRegistration = User.builder().username(username).email(email).password1(pw1).build();
         userRepository.save(newRegistration);
         System.out.printf("Registered: %s %s %s %n", username, email, pw1);
