d9095864d180878b4eaabc3caa5c325ea25fead8
mirellaHalmai <halmaimirella@gmail.com>
2019-04-24 15:07:39 +0200

    starting controller for edit dog page

--- src/main/java/com/codecool/dogshelter/controller/DogController.java
+++ src/main/java/com/codecool/dogshelter/controller/DogController.java
@@ -123,4 +123,26 @@ public class DogController {
         return dog;
     }
 
+    @PutMapping("shelter/{shelterId}/dog/{id}")
+    void updateDogInfo(
+            @PathVariable Long shelterId,
+            @PathVariable Long id,
+            @RequestParam(name = "file", required = false) MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("breed") Breed breed,
+            @RequestParam("dateOfBirth") LocalDate dateOfBirthString,
+            @RequestParam("size") DogSize size,
+            @RequestParam(name = "status", required = false, defaultValue = "AVAILABLE") Status status,
+            @RequestParam(name = "personalityTrait", required = false) String personalityTrait,
+            @RequestParam(name = "dreamHome", required = false) String dreamHome,
+            @RequestParam(name = "specialFeatures", required = false) String specialFeatures,
+            @RequestParam("gender") Gender gender,
+            @RequestParam("isNeutered") boolean isNeutered,
+            @RequestParam(name = "ownerName", required = false) String ownerName,
+            @RequestParam(name = "ownerPhoneNumber", required = false) String ownerPhoneNumber,
+            @RequestParam(name = "ownerEmail", required = false) String ownerEmail
+            ) {
+
+    }
+
 }
